87ffdebddba0fbebf267e0e92ff93b0a77f3f8e9
Bill Allen <photo.allen@gmail.com>
2017-06-01 15:02:08 +0100

    Added download URL and keywords

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup
 # vim: expandtab tabstop=4 shiftwidth=4
 
 setup(
-    version='0.7.0',
+    version='0.7.1',
     name='pypki2',
     description='More user-friendly way to access PKI-enabled services',
-    url='',
+    url='https://github.com/nbgallery/pypki2',
     author="Bill Allen",
     author_email="photo.allen@gmail.com",
     scripts=[],
     packages=['pypki2',],
     package_data={},
-    install_requires=[]
+    install_requires=[],
+    download_url='https://github.com/nbgallery/pypki2/archive/0.7.1.tar.gz',
+    keywords=['pki','ssl','pem','pkcs12','p12']
 )
