ccf19aa6d9449f1e298d3aa1bc843d0e16a34367
Bill Allen <photo.allen@gmail.com>
2018-03-09 21:27:59 +0000

    Added tests for new patch and unpatch functionality.

--- tests/test_pypki2.py
+++ tests/test_pypki2.py
@@ -25,6 +25,29 @@ class GitHubTest(unittest.TestCase):
         d = bin_to_utf8(resp.read())
         print(d)
 
+class PatchTest(unittest.TestCase):
+    def test_patch_unpatch_patch(self):
+        pypki2.pypki2.patch()
+        self.assertTrue(pypki2.pypki2.is_patched())
+
+        pypki2.pypki2.unpatch()
+        self.assertFalse(pypki2.pypki2.is_patched())
+
+        pypki2.pypki2.patch()
+        self.assertTrue(pypki2.pypki2.is_patched())
+
+    def test_unpatch_patch_unpatch(self):
+        pypki2.pypki2.unpatch()
+        self.assertFalse(pypki2.pypki2.is_patched())
+
+        pypki2.pypki2.patch()
+        self.assertTrue(pypki2.pypki2.is_patched())
+
+        pypki2.pypki2.unpatch()
+        self.assertFalse(pypki2.pypki2.is_patched())
+
+        pypki2.pypki2.patch() # turn patching on for final tests
+
 class UserCertTest(unittest.TestCase):
     def test_good_pem_password(self):
         cert_path = 'tests/ca/user-priv-key.pem'
